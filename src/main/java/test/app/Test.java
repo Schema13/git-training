fbea16ee3848eabf2bcebf625e32879cf1e3d6c7
Philippe Lhomme <philippe.lhomme@live.fr>
2021-09-23 16:42:27 +0200

    Ajout des commentaires

--- src/main/java/test/app/Test.java
+++ src/main/java/test/app/Test.java
@@ -1,8 +1,13 @@
+/**
+ * Commentaire
+ * 
+ */
 package test.app;
 
 public class Test {
 
     public static void main(String[] args) {
+        // ----------
         System.out.println("Hello !!");
     }
-}
+}è
